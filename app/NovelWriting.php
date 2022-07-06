cc8c3abc0c0f9962a969a752177c16df38a9236d
murata <kazunari.loto6@gmail.com>
2022-07-06 02:34:52 +0000

    controller編集

--- app/NovelWriting.php
+++ app/NovelWriting.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class NovelWriting extends Model
 {
-    //
+    // 以下を追記
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'title' => 'required',
+        'body' => 'required',
+    );
 }
