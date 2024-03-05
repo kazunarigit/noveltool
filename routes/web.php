e7632b675ea1217426be0d1defff9d2a1c0590e6
murata <kazunari.loto6@gmail.com>
2024-03-05 07:11:05 +0000

    env webch

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 */
 Route::get('/', 'NovelWritingController@index');
 
-Route::controller(NovelWritingController::class)->group(['prefix' => 'admin', 'as'=> 'admin.', 'middleware' => 'auth'], function() {
+Route::group(['prefix' => 'admin', 'as'=> 'admin.', 'middleware' => 'auth'], function() {
     Route::resource('novel_writings', 'Admin\NovelWritingController');
     
     Route::get('/novel_writings/{novel_writing}/edit', 'NovelWritingController@edit')->name('novel_writings.edit');
